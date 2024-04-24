5b0becda3ed2dcd5c841926b98f440893f688777
Vamshix57 <vamshi.krishna@kalvium.community>
2024-04-24 18:13:19 +0530

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,45 @@
-import './App.css';
+import "./App.css";
 import elephant from "./images/elephant.jpeg";
 
-const imageData = ()=>{
+const imageData = () => {
   let data = [
     {
-      id:1,
-      img:elephant
+      id: 1,
+      img: elephant,
     },
     {
-      id:2,
-      img:elephant
+      id: 2,
+      img: elephant,
     },
     {
-      id:3,
-      img:elephant
+      id: 3,
+      img: elephant,
     },
     {
-      id:4,
-      img:elephant
-    }
-  ]
+      id: 4,
+      img: elephant,
+    },
+  ];
   return data;
-}
+};
 
 function App() {
-  // code here
+  let img_arr = imageData();
+
+  return (
+    <>
+      <h1>Kalvium Gallery</h1>
+      <div className="parent_div">
+        {/* <img src={img_arr[0].img} className='image_div' alt="" />
+      <img src={img_arr[1].img} className='image_div' alt="" />
+      <img src={img_arr[2].img} className='image_div' alt="" />
+      <img src={img_arr[3].img} className='image_div' alt="" /> */}
+        {img_arr.map((item) => {
+          return <img src={item.img} alt="elphant" className="image_div"></img>;
+        })}
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/AppClass.jsx
+++ src/AppClass.jsx
@@ -1,30 +1,48 @@
 import { Component } from "react";
-import "./App.css"
+import "./App.css";
 import elephant from "./images/elephant.jpeg";
 
-export default class AppClass extends Component{
-  
-  imageData = ()=>{
+export default class AppClass extends Component {
+  imageData = () => {
     let data = [
       {
-        id:1,
-        img:elephant
+        id: 1,
+        img: elephant,
       },
       {
-        id:2,
-        img:elephant
+        id: 2,
+        img: elephant,
       },
       {
-        id:3,
-        img:elephant
+        id: 3,
+        img: elephant,
       },
       {
-        id:4,
-        img:elephant
-      }
-    ]
+        id: 4,
+        img: elephant,
+      },
+    ];
     return data;
-  }
+  };
 
   // code here
+  render() {
+    let img_arr = this.imageData();
+    return (
+      <>
+        <h1>Kalvium Gallery</h1>
+        <div className="parent_div">
+          {/* <img src={img_arr[0].img} className='image_div' alt="" />
+        <img src={img_arr[1].img} className='image_div' alt="" />
+        <img src={img_arr[2].img} className='image_div' alt="" />
+        <img src={img_arr[3].img} className='image_div' alt="" /> */}
+          {img_arr.map((item) => {
+            return (
+              <img src={item.img} alt="elphant" className="image_div"></img>
+            );
+          })}
+        </div>
+      </>
+    );
+  }
 }
